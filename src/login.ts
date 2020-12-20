9f0c0f511f6b6615cfaf08b050a7dd56f8d257ac
Hasan Onat Toprak <hasan.onat.toprak@gmail.com>
2020-12-20 23:03:11 +0300

    Some fix

--- src/login.ts
+++ src/login.ts
@@ -1,4 +1,4 @@
-import {object, size, string} from "superstruct";
+import {Infer, object, size, string} from "superstruct";
 
 export const Username = size(string(), 6, 15);
 export const Password = size(string(), 8, 20);
@@ -7,3 +7,5 @@ export const LoginRequest = object({
     username: Username,
     password: Password,
 });
+
+export type LoginRequest = Infer<typeof LoginRequest>
